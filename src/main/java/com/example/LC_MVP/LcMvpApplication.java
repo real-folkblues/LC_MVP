c9ed178e746aecb8c687f05e6acaee5983f1f8dc
real-folkblues <marenicolor@gmail.com>
2024-03-10 20:36:23 -0400

    Initial commit

---
+++ src/main/java/com/example/LC_MVP/LcMvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.LC_MVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LC_MVP/LcMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LC_MVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
